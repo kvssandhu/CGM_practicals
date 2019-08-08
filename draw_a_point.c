d6e4bd414a992321204ef0fd21d763179b74e8f4
Karanveer Singh <sandhunoddy@gmail.com>
2019-08-08 19:39:05 +0530

    Test Change -1

--- draw_a_point.c
+++ draw_a_point.c
@@ -6,8 +6,9 @@ int main(){
 int gd = DETECT, gm;
 initgraph(&gd, &gm, NULL);
 for(int i=0; i<=400; i++){
-    // Code for point
-    putpixel(100,200, RED);
+    // Code for Line
+
+    putpixel(i,200, RED);
 }
 getch();
 closegraph();
