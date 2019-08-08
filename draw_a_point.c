aac63a82103cfe46920d6e021d3d636645d88eac
Karanveer Singh <sandhunoddy@gmail.com>
2019-08-08 19:25:24 +0530

    Added Draw a point Source Code

---
+++ draw_a_point.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<graphics.h>
+
+int main(){
+
+int gd = DETECT, gm;
+initgraph(&gd, &gm, NULL);
+for(int i=0; i<=400; i++){
+
+    putpixel(i,200, RED);
+}
+getch();
+closegraph();
+return 0;
+}
